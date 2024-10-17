4470c11d301df5ab1094861b7d2c3bd2832b38f3
Enzo Nguyen <enzo.brightsoft@gmail.com>
2024-10-17 14:09:09 +0700

    fix v2.0

--- Deployment/deploy.sh
+++ Deployment/deploy.sh
@@ -1,4 +1,4 @@
-﻿docker build -t lweys_api -f Dockerfile.LWEYSAPI ../LWEYSWebAPI
+﻿docker build -t lweys_api -f Dockerfile.LWEYSAPI ..
 docker-compose -f docker-compose.yml down
 docker-compose build
 docker-compose -f docker-compose.yml up -d

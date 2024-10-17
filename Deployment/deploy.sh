0aa8200e0a4ab33786a8bbc31abd577e01bfcf54
Enzo Nguyen <enzo.brightsoft@gmail.com>
2024-10-17 14:32:21 +0700

    fix config v3.0

--- Deployment/deploy.sh
+++ Deployment/deploy.sh
@@ -1,4 +1,8 @@
-﻿docker build -t lweys_api -f Dockerfile.LWEYSAPI ..
+﻿# lweys_api
+docker build -t lweys_api -f Dockerfile.LWEYSAPI ..
+# lweys
+docker build -t lweys -f Dockerfile.LWEYS ..
+
 docker-compose -f docker-compose.yml down
 docker-compose build
 docker-compose -f docker-compose.yml up -d
